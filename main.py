500c8c9dbf0318a3d277eb7385212e709afeaa7b
Hamza Zubair <gondal.humza@gmail.com>
2023-05-14 06:06:39 +0200

    Updates in output mechanism

--- main.py
+++ main.py
@@ -2,21 +2,35 @@ from crawler.page_crawler import PageCrawler
 from utils.resource_pool import ResourcePool
 
 
-if __name__ == '__main__':
-    base_url = 'bbc.co.uk'
+def crawl_website(base_url, start_url):
     crawler = PageCrawler(base_url)
-
     pool = ResourcePool(num_workers=100)
 
     def crawl_url(url):
         crawler.crawl(url)
 
-    pool.spawn_worker(crawl_url, 'https://www.bbc.co.uk')
-
+    pool.spawn_worker(crawl_url, start_url)
     pool.wait_for_free_worker()
 
     links = crawler.all_links
 
-    print(f"Found {len(links)} links")
+    results = []
     for link in links:
-        print(f"{base_url} -> {link}")
+        results.append(f"{base_url} -> {link}")
+
+    return results
+
+
+if __name__ == '__main__':
+    base_url = 'bbc.co.uk'
+    start_url = 'https://www.bbc.co.uk'
+
+    results = crawl_website(base_url, start_url)
+
+    # Print the results in the specified format
+    for result in results:
+        print(result)
+
+    print(f"Total links found: {len(results)}")
+    print(f"Total unique links found: {len(set(results))}")
+    print(f"Total duplicate links found: {len(results) - len(set(results))}")
